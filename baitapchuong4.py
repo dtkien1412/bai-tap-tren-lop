a97368cd0871eb83b17bd4052f36bc30509acb75
dtkien1412 <62270097+dtkien1412@users.noreply.github.com>
2020-05-22 08:58:38 +0700

    Add files via upload

---
+++ baitapchuong4.py
@@ -0,0 +1,18 @@
+#4.1
+values=input("Nhập vào các giá trị:")
+l=values.split(",")
+t=tuple(l)
+print (l)
+print (t)
+#4.3
+d=dict()
+for i in range(1,21):
+    d[i]=i**2
+print(d)
+#4.2
+d=dict()
+a=int(input(" so nguoi can nhap la: "))
+for i in range(0,a):
+    n_i=str(input("nhap ten: "))
+print(n_i)
+      
